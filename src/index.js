1bde0949807d30d226cfbed72d1c63606eb2d33d
kekdl <36787202+kekdl@users.noreply.github.com>
2018-03-04 13:27:37 +0300

    Update and rename src/index.js to  src/index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 module.exports = function getZerosCount(number) {
-  // your implementation
+    // your implementation
+    var num = number;
+    var result = 0;
+    var i = 0;
+    while (num > Math.pow(5, i)) {
+        i++;
+        console.log(Math.pow(5, i));
+        result = result + Math.floor(num / Math.pow(5, i));
+    }
+    return result;
 }
